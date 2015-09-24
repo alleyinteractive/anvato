a985eaa820ac495c27e55f7a9c1892f31463c363
Matthew Boynes <mboynes@alleyinteractive.com>
2015-09-24 11:32:50 -0400

    Update anvato.php

--- anvato.php
+++ anvato.php
@@ -4,10 +4,12 @@
 	Plugin URI: http://www.alleyinteractive.com/
 	Description: Anvato Video Player
 	Version: 0.1
-	Author: Matthew Boynes
+	Author: Matthew Boynes, Alley Interactive, Media General
 	Author URI: http://www.alleyinteractive.com/
 */
-/*  This program is free software; you can redistribute it and/or modify
+/*  Copyright (c) 2015 Media General
+
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
